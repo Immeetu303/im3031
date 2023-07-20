a7e40dbf8cb566208f7f471b42d2e6ce7774c29d
SAMSUNG <IMMEETU303@GMAIL.COM>
2023-07-20 09:30:29 +0530

    im303 added

---
+++ src/main/kotlin/im303.kt
@@ -0,0 +1,29 @@
+fun main() {
+    println("Hello Word")
+    val intValue: Int = 45
+    println("Integer value: $intValue")
+    // Float
+    val floatValue: Float = 0.25f
+    println("Float value: $floatValue")
+    // Char
+    val charValue: Char = 'A'
+    println("Char value: $charValue")
+    //string
+    val stringValue: String = "Meet"
+    println("String value: $stringValue")
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean value: $booleanValue")
+    // Double
+    val doubleValue: Double = 3.14
+    println("Double value: $doubleValue")
+    // Long
+    val longValue: Long = 987654321L
+    println("Long value: $longValue")
+    // Short
+    val shortValue: Short = 32767
+    println("Short value: $shortValue")
+    // Byte
+    val byteValue: Byte = 127
+    println("Byte value: $byteValue")
+}
